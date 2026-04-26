9d76db7550d28bc5f0b9a1f0b058b405443bd6f0
sourabhsthavaramath <sourabhbs09@gmail.com>
2026-04-27 00:48:12 +0530

    Remove unused function declaration

--- Core/Inc/lcd.h
+++ Core/Inc/lcd.h
@@ -1,7 +1,13 @@
+/*
+ *NAME: SOURABH B STHAVARAMATH
+ *DATE: 12-01-2025
+ *FILE NAME: lcd.h
+ */
+
 void KM_LCD_Init(void);
 void KM_LCD_Write_Cmd(unsigned char);
 void KM_LCD_Write_Data(unsigned char);
 void write_high_nibble(unsigned char);
 void write_low_nibble(unsigned char);
 void KM_LCD_Write_Str(char[]);
-//char* my_i_to_a(short int number);
+
